6e73cfcc8852574bc4cc0187c471375a053f421b
pallaviB65 <pallavibharti5@gmail.com>
2020-04-14 15:33:10 +0530

    handling layout with splitscreen code -- @Ketki

--- app/src/main/java/com/example/languagesupportapp/SecondActivity.java
+++ app/src/main/java/com/example/languagesupportapp/SecondActivity.java
@@ -1,15 +1,22 @@
 package com.example.languagesupportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
-public class SecondActivity extends BaseActivity {
+public class SecondActivity extends BaseActivity implements MyRecyclerViewAdapter.ItemClickListener {
     TextView helloTV;
+    MyRecyclerViewAdapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,48 @@ public class SecondActivity extends BaseActivity {
 
         helloTV = findViewById(R.id.helloTV);
         helloTV.setText(getApplicationContext().getResources().getString(R.string.hello));
+
+        // data to populate the RecyclerView with
+        ArrayList<String> viewNames = new ArrayList<>();
+        viewNames.add("View 1");
+        viewNames.add("View 2");
+        viewNames.add("View 3");
+        viewNames.add("View 4");
+        viewNames.add("View 5");
+        viewNames.add("View 6");
+        viewNames.add("View 7");
+        viewNames.add("View 8");
+        viewNames.add("View 9");
+        viewNames.add("View 10");
+
+        // set up the RecyclerView
+        recyclerView = findViewById(R.id.rvViews);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, viewNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
+    @Override
+    public void onItemClick(View view, int position) {
+        //Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onConfigurationChanged(final Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setupRecyclerView(getRecyclerViewOrientation(newConfig.orientation));
+    }
+
+    private int getRecyclerViewOrientation(final int orientation) {
+        return isOrientationPortrait(orientation) ? LinearLayoutManager.VERTICAL : LinearLayoutManager.HORIZONTAL;
+    }
+
+    private boolean isOrientationPortrait(final int orientation) {
+        return orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+
+    private void setupRecyclerView(final int orientation) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, orientation, false));
+        recyclerView.setAdapter(adapter);
+    }
+
 }
